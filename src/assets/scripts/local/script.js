815c45f88d8eb27ce42b32e689f879dab0387490
Oleg Kharyton <olegkhacv@gmail.com>
2022-05-18 23:38:11 +0300

    feat: add swiper layout

---
+++ src/assets/scripts/local/script.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const swiperCustomers = document.querySelector('.swiper-customers');
+
+if (swiperCustomers) {
+    const swiper = new Swiper('.swiper-customers', {
+        // Optional parameters
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+}
