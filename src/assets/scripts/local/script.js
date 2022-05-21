8794d1069606dd935e0ac72ddb4382584eac2cab
Oleg Kharyton <olegkhacv@gmail.com>
2022-05-21 16:44:39 +0300

    fix: change bullets styles and properties

--- src/assets/scripts/local/script.js
+++ src/assets/scripts/local/script.js
@@ -5,11 +5,13 @@ const swiperCustomers = document.querySelector('.swiper-customers');
 if (swiperCustomers) {
     const swiper = new Swiper('.swiper-customers', {
         // Optional parameters
+        autoheight: true,
         loop: true,
 
         // If we need pagination
         pagination: {
             el: '.swiper-pagination',
+            clickable: true
         },
     });
 }
